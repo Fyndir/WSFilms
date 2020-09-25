83adb14b8b9cccf7978282063ca3505f62402433
Etudiant <Etudiant@DESKTOP-3LADFE1>
2020-09-25 17:18:13 +0200

    feature : add archi uwp

---
+++ WSFILM/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,24 @@
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSFILM.ViewModel
+{
+    class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<VMHomePage>();
+        }
+
+        /// <summary>
+        /// Gets the Main property.
+        /// </summary>
+        public VMHomePage HomePage => ServiceLocator.Current.GetInstance<VMHomePage>();
+    }
+}
